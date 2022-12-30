160b1f32add96d678ba0b14f356f2ce8644e34da
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-30 03:47:38 -0300

    move: move bootstrap file to src/main

--- src/main/bootstrap.ts
+++ src/main/bootstrap.ts
@@ -1,9 +1,16 @@
 import Fastify from 'fastify'
+import { userRoutes } from './routes/user/userRoutes'
 
 const PORT = 4000
 
 const app = Fastify({
   logger: true
+})
+
+app.register(userRoutes, { prefix: '/user' })
+
+app.post('/', async (req, res) => {
+  await res.status(201).send(req.body)
 })
 
 app.listen({
